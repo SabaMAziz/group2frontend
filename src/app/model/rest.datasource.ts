182c907c81b45aa3681b730d35cd329fc0c43eb6
zack <98119870+zikkri@users.noreply.github.com>
2022-08-13 14:35:07 -0500

    rabdom push

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -10,8 +10,8 @@ import { ResponseModel } from "./response.model";
 import { environment } from "src/environments/environment";
 
 
-const PROTOCOL = "http";
-const PORT = 3000;
+// const PROTOCOL = "http";
+// const PORT = 3000;
 
 @Injectable()
 export class RestDataSource {
@@ -20,7 +20,7 @@ export class RestDataSource {
     auth_token: string;
 
     constructor(private http: HttpClient) {
-        this.baseUrl = this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+        this.baseUrl = environment.apiurl;
     }
 
     getTicketList(): Observable<Tickets[]> {
